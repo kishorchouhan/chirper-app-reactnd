fa81199c0240fac49acf92868641376826a792b1
kishorchouhan <devkishorbwr@gmail.com>
2020-03-18 19:16:54 +0530

    Updates

---
+++ precache-manifest.6a1f7d077182446b5f25f1865dfeeee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d773028584c1b618ae7afc8cf0a727",
+    "url": "/chirper-app-reactnd/index.html"
+  },
+  {
+    "revision": "4ce2b889f8d220e5ee1c",
+    "url": "/chirper-app-reactnd/static/css/main.c0322f3e.chunk.css"
+  },
+  {
+    "revision": "3c1195fcdcbb3b7c660f",
+    "url": "/chirper-app-reactnd/static/js/2.e2ddf368.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/chirper-app-reactnd/static/js/2.e2ddf368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce2b889f8d220e5ee1c",
+    "url": "/chirper-app-reactnd/static/js/main.27393cbe.chunk.js"
+  },
+  {
+    "revision": "ea7a0738f89fb1ce1a4a",
+    "url": "/chirper-app-reactnd/static/js/runtime-main.5541a6c9.js"
+  }
+]);
